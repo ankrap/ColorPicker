5e9fea6264f7e0d8d0adab46a734238ae94963a5
user <user@example.com>
2023-05-08 18:00:55 +0300

    ColorPicker initial

--- ColorPicker/ViewController.swift
+++ ColorPicker/ViewController.swift
@@ -9,11 +9,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var ColorView: UIView!
+    
+    @IBOutlet var RedSlider: UISlider!
+    @IBOutlet var GreenSlider: UISlider!
+    @IBOutlet var BlueSlider: UISlider!
+
+    @IBOutlet var RedValueLabel: UILabel!
+    @IBOutlet var GreenValueLabel: UILabel!
+    @IBOutlet var BlueValueLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupViews()
+        refreshColor()
     }
 
-
+    @IBAction func redValueChanged() {
+        RedValueLabel.text = String(format: "%.2f", RedSlider.value)
+        refreshColor()
+    }
+        
+    @IBAction func greenValueChanged() {
+        GreenValueLabel.text = String(format: "%.2f", GreenSlider.value)
+        refreshColor()
+    }
+    
+    @IBAction func BlueValueChanged() {
+        BlueValueLabel.text = String(format: "%.2f", BlueSlider.value)
+        refreshColor()
+    }
+    
+    private func setupViews() {
+        ColorView.layer.cornerRadius = 15.0
+        
+        redValueChanged()
+        greenValueChanged()
+        BlueValueChanged()
+    }
+    
+    private func refreshColor()
+    {
+        ColorView.backgroundColor? = UIColor(
+            red: CGFloat(RedSlider.value),
+            green: CGFloat(GreenSlider.value),
+            blue: CGFloat(BlueSlider.value),
+            alpha: 1.0
+        )
+    }
+    
 }
 
